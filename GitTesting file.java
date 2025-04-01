d44157a0c04f360c29920d2bccf28fee56f7f1a7
regankattel <106126833+regankattel@users.noreply.github.com>
2025-04-01 18:22:11 -0500

    Delete GitTesting file.java
    testing delete file

--- GitTesting file.java
+++
@@ -1,8 +0,0 @@
-        List appNames = new ArrayList();
-		appNames.add("Active Directory - HQ");
-		appNames.add("Google Workspace - UBIF");
-		appNames.add("Slack");
-		appNames.add("Google Workspace - Twilio");
-		if(links!= null && Util.nullSafeSize(links) >0 ) {
-	    	  
-			  if(appNames != null && Util.nullSafeSize(appNa
